f400123d689afd4e8e031b877ba04936e7601aae
Xian-p <xianrichbatayola@gmail.com>
2026-02-22 06:37:55 +0800

    Refactor command registration and improve onDisable

--- src/main/java/com/dola/chips/Chips.java
+++ src/main/java/com/dola/chips/Chips.java
@@ -1,33 +1,53 @@
 package com.dola.chips;
 
+import org.bukkit.command.PluginCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Chips extends JavaPlugin {
+
     private AuthManager authManager;
+
+    // Used for PersistentDataContainer key in AuthManager
     public static final String NAMESPACED_KEY = "chips_auth";
 
     @Override
     public void onEnable() {
         saveDefaultConfig();
+
         authManager = new AuthManager(this);
-        
+
+        // Events
         getServer().getPluginManager().registerEvents(new AuthListener(this), this);
-        getCommand("register").setExecutor(new AuthCommand(this));
-        getCommand("login").setExecutor(new AuthCommand(this));
-        getCommand("changepassword").setExecutor(new AuthCommand(this));
-        getCommand("resetpassword").setExecutor(new AuthCommand(this));
-        
+
+        // Commands (one executor instance for all)
+        AuthCommand authCommand = new AuthCommand(this);
+
+        registerExecutor("register", authCommand);
+        registerExecutor("login", authCommand);
+        registerExecutor("changepassword", authCommand);
+        registerExecutor("resetpassword", authCommand);
+
         getLogger().info("Chips Auth enabled!");
     }
 
     @Override
     public void onDisable() {
-        authManager.saveData();
+        if (authManager != null) {
+            authManager.saveData();
+        }
         getLogger().info("Chips Auth disabled!");
+    }
+
+    private void registerExecutor(String commandName, AuthCommand executor) {
+        PluginCommand pc = getCommand(commandName);
+        if (pc != null) {
+            pc.setExecutor(executor);
+        } else {
+            getLogger().warning("Command '" + commandName + "' not found in plugin.yml");
+        }
     }
 
     public AuthManager getAuthManager() {
         return authManager;
     }
-  }
-
+        }
